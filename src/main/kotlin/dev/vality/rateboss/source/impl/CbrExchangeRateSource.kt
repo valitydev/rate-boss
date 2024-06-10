5c56f49c8b38fc779de7ddfb31e723f07d1b33f2
Gregory <32060161+ggmaleva@users.noreply.github.com>
2024-06-10 12:56:17 +0300

    Fix/cp 22 (#14)
    * fix currency
    
    * fix time for job and timestamp
    
    * revert job time
    
    * revert job time (2)
    
    * review
    
    ---------
    
    Co-authored-by: ggmaleva <ggmaleva@yandex.ru>

--- src/main/kotlin/dev/vality/rateboss/source/impl/CbrExchangeRateSource.kt
+++ src/main/kotlin/dev/vality/rateboss/source/impl/CbrExchangeRateSource.kt
@@ -9,7 +9,7 @@ import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 import java.time.Instant
-import java.time.ZoneOffset
+import java.time.ZoneId
 
 private val log = KotlinLogging.logger {}
 
@@ -33,7 +33,7 @@ class CbrExchangeRateSource(
         val rates: Map<String, BigDecimal> = response.currencies!!.associate {
             it.charCode!! to it.value!!.divide(it.nominal!!.toBigDecimal())
         }
-        val nextDayTimestamp = response.date!!.plusDays(1).atStartOfDay().toEpochSecond(ZoneOffset.UTC)
+        val nextDayTimestamp = response.date!!.atStartOfDay(ZoneId.of("Europe/Moscow")).toEpochSecond()
         log.info("Exchange rates from cbr have been retrieved, time=$time, exchangeRates=$rates, targetTimestamp=$nextDayTimestamp")
         return ExchangeRates(
             rates = rates,
